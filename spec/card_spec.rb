2ad7e198a55bd661dbabff5785de736bf786af06
Fermin <fermin.hdez@gmail.com>
2020-05-27 17:00:51 +0200

    Use of let variables

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
   def initialize rank, suit
     @rank = rank
     @suit = suit
@@ -11,10 +11,20 @@ RSpec.describe Card do
   #  @card = Card.new('Ace', 'Spades')
   # end
 
-  def card
-    Card.new('Ace', 'Spades')
-  end
+  # def card
+  #   Card.new('Ace', 'Spades')
+  # end
 
+  # picks a symbol and create de variable
+  # the block represent de equal
+  let(:card) { Card.new('Ace', 'Spades')}
+
+  it 'has a rank and that rank can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+  
   it 'has a rank' do
     expect(card.rank).to eq('Ace')
   end

975204fa5b41a1d92316b8ac6ce227771aebe8c5
Fermin <fermin.hdez@gmail.com>
2020-05-27 17:03:54 +0200

    Add tests with error messages

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -32,6 +32,11 @@ RSpec.describe Card do
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
   end
+
+  it 'has a custom error message' do
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison}"
+  end
 end
 
 

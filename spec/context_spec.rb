48dac68411fce4e5497907b72cc7c857e7225578
Fermin <fermin.hdez@gmail.com>
2020-05-27 17:50:33 +0200

    add example before and after spec

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe '#even? method' do
   # it 'should return true if number is even' do
   # it 'should return false if number is odd' do
-  describe 'with even number' do
+  context 'with even number' do
     it 'should return true' do
       expect(4.even?).to eq(true)
     end
   end
 
-  describe 'with odd number' do
+  context 'with odd number' do
     it 'should return false' do
       expect(5.even?).to eq(false)
     end
